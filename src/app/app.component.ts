51855b34cb2d6e8dfdabc611e541ad75d5f7430d
Taimoor Tariq <taimoor_t@yahoo.com>
2019-04-19 02:55:47 +0500

    icons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-apps';
+  playpause = 'play'
+  videLink = 'https://www.youtube.com/watch?v=yx-HYerClEA'
+  @ViewChild('videoPlayer') videoplayer: ElementRef;
+  videoWidth;
+
+  constructor(private elementRef:ElementRef){}
+
+  ngAfterViewInit(){
+    this.videoplayer.nativeElement.addEventListener('timeupdate', this.onClick.bind(this));
+  }
+
+  onClick(event){
+    // console.log("event",event);
+    let juice = this.videoplayer.nativeElement.currentTime / this.videoplayer.nativeElement.duration;
+    // console.log("juice =>",(juice*100+'%'));
+    this.videoWidth = juice*100+'%';
+
+    if(this.videoplayer.nativeElement.ended){
+      this.playpause = 'play'
+    }
+
+  }
+
+  toggleVideo(event: any) {
+    // console.log("check")
+    if( this.videoplayer.nativeElement.paused){
+      this.videoplayer.nativeElement.play();
+      this.playpause = 'pause';
+    }
+    else{
+      this.videoplayer.nativeElement.pause();
+      this.playpause = 'play'
+    }
+
+}
+
 }
